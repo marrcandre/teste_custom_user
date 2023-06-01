4c99e32989c6fb860b872c08d32d41a4b65db52a
Marco André Mendes <marcoandre@gmail.com>
2023-06-01 11:29:51 -0300

    initial commit

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+#model para categoria   
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
+    
---
+++ usuarios/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import Usuario
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Usuario
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            _("Aditional Information"),
+            {"fields": ("cpf", "telefone", "data_nascimento")},
+        ),
+        (
+            _("Permissions"),
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+    readonly_fields = ["date_joined", "last_login"]
+
+
+admin.site.register(Usuario, CustomUserAdmin)
