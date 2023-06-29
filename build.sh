ee76c0efd5e7e7e0b4da540315a9bc9490659d63
Marco André Mendes <marcoandre@gmail.com>
2023-06-29 11:31:57 -0300

    downgrade dango

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-/opt/render/project/src/.venv/bin/python -m pip install poetry
 
-# poetry install
-pip install -r requirements.txt
+# /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+
+poetry install
+# pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
