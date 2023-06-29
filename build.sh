a7a9085f6f593376974500d33556facd6a41f7ba
Marco André Mendes <marcoandre@gmail.com>
2023-06-29 11:26:37 -0300

    pip upgrade

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+/opt/render/project/src/.venv/bin/python -m pip install poetry
 
 # poetry install
 pip install -r requirements.txt
